3271fa1efa0d4f89978241f59b16efcdf9e1d78d
EsGoreng <itsnaakhdan25@gmail.com>
2025-03-16 23:48:01 +0700

    Program and Algorithm Activity Courses

---
+++ Aktivitas Matkul Algoritma dan Pemrograman/Aktivitas 3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Itsna Akhdan Fadhil - 102022400056
+
+int terbesar_0056;
+
+    if (85 > 75 && 85 > 65) {
+        terbesar_0056 = 85;
+    }
+    else if (75 > 65) {
+        terbesar_0056 = 75;
+    }
+    else if (85 == terbesar_0056) {
+        Console.Write("85 adalah terbesar");
+    }
+    else {
+        Console.Write("85 bukan yang terbesar");
+    }
---
+++ Aktivitas Matkul Algoritma dan Pemrograman/Aktivitas 6/Tugas Alpro Perulangan/Python/Python.py
@@ -0,0 +1,4 @@
+nilai_0056 = 1;
+while (nilai_0056 <= 5):
+    print(nilai_0056)
+    nilai_0056+=1
